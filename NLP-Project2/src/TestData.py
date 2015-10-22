3082b8429f713efe0cd24415578aa848079f6ee0
Mihir <mihir.5191@gmail.com>
2015-10-22 19:06:30 -0400

    added smoothing and got accuracy of 54.26
    Change-Id: I684bd907acf0ebea99c7e4507598a7f2f032fe6f

--- NLP-Project2/src/TestData.py
+++ NLP-Project2/src/TestData.py
@@ -12,6 +12,7 @@ from nltk.corpus import stopwords
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.corpus import wordnet as wn
 import csv
+from XMLParser import temp_dictionary1
 
 
 
@@ -31,7 +32,10 @@ senseIdCount={}
 context_dictionary={}
 window_size=15           
 stop = stopwords.words('english')
-
+lexeltContext={}
+Total_contexts={}
+lexeltSenseIdMap={}
+senseIdLexeltMap={}
 
 '''
 The following loop parses the xml document and computes the following things
@@ -44,6 +48,8 @@ for lexelt in lexelts :
     #instances=doc.getElementsByTagName("instance")
     instances=lexelt.getElementsByTagName("instance")
     item_name = lexelt.getAttribute("item")
+    
+    lexeltSenseIdMap[item_name]=[]
     
     
     count=0; 
@@ -76,10 +82,16 @@ for lexelt in lexelts :
         
         for answer in answers:
             senseid=answer.getAttribute("senseid")
+            
+            
+            
             if senseid == 'U':
                 senseid= senseid+item_name
-              
-                  
+            
+            senseIdLexeltMap[senseid]=item_name  
+            temp_list=lexeltSenseIdMap[item_name]
+            if senseid not in temp_list:
+                lexeltSenseIdMap[item_name].append(senseid)      
             
             if senseid in senseid_probability_for_a_word:
                 senseid_probability_for_a_word[senseid]+=1
@@ -90,39 +102,90 @@ for lexelt in lexelts :
             
             context_data=firstChild_context_elements_stemmed+lastChild_context_elements_stemmed
             #print(context_data)
+            if item_name in Total_contexts:
+                Total_contexts[item_name]= Total_contexts[item_name]+ context_data
+            else:
+                Total_contexts[item_name]=context_data 
+            
             if senseid in context_dictionary:    
                 context_dictionary[senseid]= context_dictionary[senseid]+ context_data
             else:
-                context_dictionary[senseid]= firstChild_context_elements_stemmed+lastChild_context_elements_stemmed   
+                context_dictionary[senseid]= context_data   
             count+=1
     senseid_probability_for_a_word.update((x, y/count) for x, y in senseid_probability_for_a_word.items())
     senseidProbability[item_name]=senseid_probability_for_a_word
     
 
     #print( "item name:%s, count: %s " %  (item_name, count))
-#print(lexeltSenseIdMap)
-#print("senseid probability=")
-#print(senseidProbability)
+
+f = open("D:\\MEng folders\\NLP\\project2\\lexeltSenseidMap.txt",'w')
+f.write(str(lexeltSenseIdMap))
+f.close()
+
+f = open("D:\\MEng folders\\NLP\\project2\\senseidLexeltMap.txt",'w')
+f.write(str(senseIdLexeltMap))
+f.close()
 
 f = open("D:\\MEng folders\\NLP\\project2\\senseidProb.txt",'w')
-
 f.write(str(senseidProbability))
 f.close()
-#print("senseid count=")
+
 f = open('D:\\MEng folders\\NLP\\project2\\senseIdCount.txt','w')
 f.write(str(senseIdCount))
 f.close()
-#print(senseIdCount)
-#print("context dictionary =")
+
 f = open('D:\\MEng folders\\NLP\\project2\\context_dictionaryRaw.txt','w')
 f.write(str(context_dictionary))
 f.close()
-#print(context_dictionary)
-#print("-----------------------------")
-#print(senseIdCount) 
+
 print("Id,Prediction")
 
 
+
+
+'''
+This makes the unseen words dictionary for a particular senseid in the context
+
+'''
+context_dictionary_unseen={}
+Total_contexts_unique={}
+for item_name in Total_contexts:
+    list_words=Total_contexts[item_name]
+    list_words_unique=[]
+    senseids=lexeltSenseIdMap[item_name]
+    
+    for word in list_words:
+        if word not in list_words_unique:
+            list_words_unique.append(word)
+    
+    
+    Total_contexts_unique[item_name]=list_words_unique
+    
+    for senseid in senseids:
+        sense1=context_dictionary[senseid]
+        unseen= [item for item in list_words_unique if item not in sense1]
+        context_dictionary_unseen[senseid]= unseen   
+        
+
+
+
+
+
+    
+f = open('D:\\MEng folders\\NLP\\project2\\totalContexts.txt','w')
+f.write(str(Total_contexts))
+f.close()
+
+    
+f = open('D:\\MEng folders\\NLP\\project2\\totalContextsUnique.txt','w')
+f.write(str(Total_contexts_unique))
+f.close()
+
+    
+f = open('D:\\MEng folders\\NLP\\project2\\context_dictionaryUnseenRaw.txt','w')
+f.write(str(context_dictionary_unseen))
+f.close()
+             
 '''
 The following loop does the following :
 1. Converts the context dictionary in which value was a list into the dictionary of dictionary
@@ -139,22 +202,76 @@ for senseid in context_dictionary:
             temp_dictionary[item]+=1
         else:
             temp_dictionary[item]=1
+    temp_dictionary.update((x, y+1) for x, y in temp_dictionary.items())        
     context_dictionary[senseid]=temp_dictionary
     
     #temp_dictionary.update((x, y/senseIdCount[senseid]) for x, y in temp_dictionary.items())
     #trainingContextProbability[senseid]=temp_dictionary
 
 
+
+
+
 f = open('D:\\MEng folders\\NLP\\project2\\context_dictionaryCount.txt','w')
 f.write(str(context_dictionary)) 
 f.close()    
 
+'''
+The following loop does the following :
+1. Converts the context dictionary_unseen in which value was a list into the dictionary of dictionary
+'''
+
+for senseid in context_dictionary_unseen:
+    temp_list=context_dictionary_unseen[senseid]
+    temp_dictionary={} 
+    for item in temp_list:
+        temp_dictionary[item]=1
+    context_dictionary_unseen[senseid]=temp_dictionary
+    
+    #temp_dictionary.update((x, y/senseIdCount[senseid]) for x, y in temp_dictionary.items())
+    #trainingContextProbability[senseid]=temp_dictionary
+
+
+
+
+
+f = open('D:\\MEng folders\\NLP\\project2\\context_dictionaryCount_unseen.txt','w')
+f.write(str(context_dictionary_unseen)) 
+f.close() 
+
+
+''' this combines the context_dictionary and the context_dictionary_unseen  
+'''
+
+for senseid in context_dictionary:
+    temp1 = context_dictionary[senseid].copy()
+    temp1.update(context_dictionary_unseen[senseid])
+    context_dictionary[senseid]=temp1
+#     if senseid == '38201':
+#         f = open('D:\\MEng folders\\NLP\\project2\\combinedContextDictionary38201.txt','w')
+#         f.write(str(context_dictionary[senseid])) 
+#         f.close() 
+
+f = open('D:\\MEng folders\\NLP\\project2\\context_dictionaryCombined.txt','w')
+f.write(str(context_dictionary)) 
+f.close()  
+
+
+
+'''
+Following loop calculates the probability for the training contexts feature vectors
+'''
+
 for senseid in context_dictionary:
      temp_dictionary1={}
      temp_dictionary1=context_dictionary[senseid]
+     lexelt=senseIdLexeltMap[senseid]
+#     count= len(Total_contexts[lexelt])+ len(Total_contexts_unique[lexelt])
      count=senseIdCount[senseid]
-     for word in temp_dictionary1:
-         temp_dictionary1[word]= temp_dictionary1[word]/count
+#     for word in temp_dictionary1:
+#          temp_dictionary1[word]= temp_dictionary1[word]/count
+     temp_dictionary1.update((x, y/count) for x, y in temp_dictionary1.items())     
+     
      
 
 #print(context_dictionary)
@@ -167,9 +284,20 @@ f.close()
 
 print("done outputting to a file")
 
-f = open('D:\\MEng folders\\NLP\\project2\\Prediction21.csv','w')
+f2 = open('D:\\MEng folders\\NLP\\project2\\temp_sums.txt','w')
+
+f3 = open('D:\\MEng folders\\NLP\\project2\\featureVectors.txt','w')
+
+
+f = open('D:\\MEng folders\\NLP\\project2\\Prediction321.csv','w')
 fcsv = csv.writer(f, delimiter=',')
 fcsv.writerows("Id,Prediction")
+
+
+
+
+
+
 
 for lexelt in lexelts_test :
     #instances=doc.getElementsByTagName("instance")
@@ -217,7 +345,10 @@ for lexelt in lexelts_test :
         feature_vector_stemmed=[LancasterStemmer().stem(word) for word in feature_vector]
         #print(feature_vector_stemmed)
         #print(PorterStemmer().stem_word(contexts.data))
-        
+        f3.write(','.join(feature_vector_stemmed)+ "for instanceid "+ str(instance.getAttribute("id")) + "\n")
+#         if senseid == 'different%3:00:00::':
+#             print('feature vector for sense id'+ senseid)
+#             print(feature_vector_stemmed)
         
         
         senseids=senseidProbability[item_name]
@@ -235,6 +366,8 @@ for lexelt in lexelts_test :
                 temp_sum=temp_sum*senseids[senseid]
                                
                 #print(temp_sum," ", senseid," ",end='')
+                f2.write(str(temp_sum) + " " + str(senseid) + " " )
+                
                 
                 if senseid == "U" + item_name:
                     senseid="U"
@@ -252,14 +385,16 @@ for lexelt in lexelts_test :
                              senseidLabel=senseid
 
          
-        
-        
+        f2.write("for instance" + str(instance.getAttribute("id")))
+        f2.write("\n")
         fcsv.writerows(str(instance.getAttribute("id"))+ "," +str(senseidLabel)) 
             
 
                          
         print(instance.getAttribute("id")+"\t"+senseidLabel)
 f.close()
+f2.close()
+f3.close()
         #print(senseidLabel)
         #print(PorterStemmer().stem_word(contexts.data))
 
